1fb923eb4bbb5266a7db7019c4d3adfd4a71e17f
greeshmaa965-cell <greeshmaa965@gmail.com>
2026-02-19 17:10:06 +0530

    Add linear search implementation in Java
    Implemented linear search algorithm to find target index.

---
+++ Arrays/linear search.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int linearSearch(int nums[], int target)
+    {
+		 for(int i=0;i<num.length;i++)
+     {
+      if(num[i]==target)
+      {
+        return i;
+      }
+     }
+     return -1;
+    }
+}
