ecf5550b233dd2b120671fd1d7cf7ce27d310f75
greeshmaa965-cell <greeshmaa965@gmail.com>
2026-05-10 12:17:57 +0530

    Added majorityElement(2) method to find majority elements
    Implement majority element algorithm to find elements appearing more than n/3 times.

---
+++ Arrays/majority element 2.java
@@ -0,0 +1,59 @@
+import java.util.*;
+class Solution {
+    public List<Integer> majorityElement(int[] nums)
+    {
+        List<Integer> list=new ArrayList<>();
+         int candidate1=0;
+         int candidate2=0;
+         int count1=0;
+         int  count2=0;
+         for(int num:nums)
+         {
+            if(num==candidate1)
+            {
+                count1++;
+            }
+            else if(num==candidate2)
+            {
+                count2++;
+            }
+            else if(count1==0)
+            {
+                candidate1=num;
+                count1++;
+            }
+            else if(count2==0)
+            {
+                candidate2=num;
+                count2++;
+            }
+            else{
+                count2--;
+                count1--;
+            }
+         }
+         count1=0;
+         count2=0;
+         for(int num:nums)
+         {
+            if(num==candidate1)
+            {
+                count1++;
+            }
+           else if(num==candidate2)
+            {
+                count2++;
+            }
+         }
+         int n=nums.length;
+         if(count1>n/3)
+         {
+            list.add(candidate1);
+         }
+         if(count2>n/3)
+         {
+            list.add(candidate2);
+         }
+         return list;
+    }
+}
