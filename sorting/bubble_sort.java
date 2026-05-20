1d2e55e1600abdf60e33f4ef01cf588ac1ebb2e9
greeshmaa965-cell <greeshmaa965@gmail.com>
2026-05-20 10:30:07 +0530

    Implemented bubble sort algorithm in Java

---
+++ sorting/bubble_sort.java
@@ -0,0 +1,21 @@
+class Solution {
+
+    public int[] bubbleSort(int[] nums) {
+
+        for(int i = 0; i < nums.length - 1; i++) {
+
+            for(int j = 0; j < nums.length - i - 1; j++) {
+
+                if(nums[j] > nums[j + 1]) {
+
+                    // Swap
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+        }
+
+        return nums;
+    }
+}
