191325d6c52a7c1ea96c37548f29d134c39d75f8
greeshmaa965-cell <greeshmaa965@gmail.com>
2026-05-25 12:39:13 +0530

    Implemented Dutch National Flag algorithm to sort colors

--- Arrays/sortcolors.java
+++ Arrays/sortcolors.java
@@ -16,3 +16,45 @@ class Solution {
         
     }
 }
+
+//dutch flag algorithm
+
+class Solution {
+
+    public void sortColors(int[] nums) 
+    {
+        int low = 0;
+        int mid = 0;
+        int high = nums.length - 1;
+
+        while(mid <= high)
+        {
+            // Case 1 -> 0
+            if(nums[mid] == 0)
+            {
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+
+                low++;
+                mid++;
+            }
+
+            // Case 2 -> 1
+            else if(nums[mid] == 1)
+            {
+                mid++;
+            }
+
+            // Case 3 -> 2
+            else
+            {
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+
+                high--;
+            }
+        }
+    }
+}
