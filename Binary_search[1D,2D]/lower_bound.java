d6eca3175da2c675f0f669fc99b6ef38a7ba0d86
greeshmaa965-cell <greeshmaa965@gmail.com>
2026-05-25 10:29:33 +0530

    Added lowerBound method to find lower bound index
    Implement lower bound function to find the index of the first element not less than x.

---
+++ Binary_search[1D,2D]/lower_bound.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int lowerBound(int[] nums, int x) 
+    {
+       for(int i=0;i<nums.length;i++)
+       {
+         if(nums[i]>=x)
+         {
+            return i;
+            
+         }
+       }
+       return nums.length;
+    }
+}
