a19fc6bd1bdea6247ff6db0b97232857bee64ed9
greeshmaa965-cell <greeshmaa965@gmail.com>
2026-05-25 10:32:05 +0530

    Implemented upper bound function in upper_bound.java

---
+++ Binary_search[1D,2D]/upper_bound.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int upperBound(int[] nums, int x) 
+    {
+       for(int i=0;i<nums.length;i++)
+       {
+          if(nums[i]>x)
+          {
+            return i;
+          }
+       }
+       return nums.length;
+    }
+}
