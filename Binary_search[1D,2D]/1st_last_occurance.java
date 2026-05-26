dd81a7a3ecec6b691c5a73f2c1f6ae7842358bd5
greeshmaa965-cell <greeshmaa965@gmail.com>
2026-05-26 10:13:03 +0530

    Implemented binary search for first and last occurrence

---
+++ Binary_search[1D,2D]/1st_last_occurance.java
@@ -0,0 +1,68 @@
+class Solution {
+
+    public int[] searchRange(int[] nums, int target) {
+
+        int first = firstOccurrence(nums, target);
+        int last = lastOccurrence(nums, target);
+
+        return new int[]{first, last};
+    }
+
+    // Find first occurrence
+    public int firstOccurrence(int[] nums, int target) {
+
+        int low = 0;
+        int high = nums.length - 1;
+
+        int ans = -1;
+
+        while(low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if(nums[mid] == target) {
+                ans = mid;
+                high = mid - 1; // move left
+            }
+
+            else if(nums[mid] < target) {
+                low = mid + 1;
+            }
+
+            else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    // Find last occurrence
+    public int lastOccurrence(int[] nums, int target) {
+
+        int low = 0;
+        int high = nums.length - 1;
+
+        int ans = -1;
+
+        while(low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if(nums[mid] == target) {
+                ans = mid;
+                low = mid + 1; // move right
+            }
+
+            else if(nums[mid] < target) {
+                low = mid + 1;
+            }
+
+            else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+}
