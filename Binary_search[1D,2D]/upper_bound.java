a33b5048302c02c4e0429247049d342bbf26c89a
greeshmaa965-cell <greeshmaa965@gmail.com>
2026-05-28 11:09:23 +0530

    Added optimal upper bound method using binary search
    Implemented an optimal solution for finding the upper bound of a target in a sorted array using binary search.

--- Binary_search[1D,2D]/upper_bound.java
+++ Binary_search[1D,2D]/upper_bound.java
@@ -11,3 +11,35 @@ class Solution {
        return nums.length;
     }
 }
+
+
+//////////////////////////////////////////////////////optimal solution///////////////////////////////////////////////////////////////////
+class Solution {
+
+    public int upperBound(int[] arr, int target)
+    {
+        int low = 0;
+        int high = arr.length - 1;
+
+        int ans = arr.length;
+
+        while(low <= high)
+        {
+            int mid = low + (high - low) / 2;
+
+            // possible answer
+            if(arr[mid] > target)
+            {
+                ans = mid;
+                high = mid - 1;
+            }
+
+            else
+            {
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+}
