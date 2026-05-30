007f35e305fc91532ab19c878a58049e2bbdd46c
greeshmaa965-cell <greeshmaa965@gmail.com>
2026-05-30 11:24:28 +0530

    Implemented binary search to find minimum in rotated array

---
+++ Binary_search[1D,2D]/search_min_rotated_sorted_array.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int findMin(int[] nums) {
+
+        int low = 0;
+        int high = nums.length - 1;
+        int ans = Integer.MAX_VALUE;
+
+        while(low <= high) {
+
+            // If current search space is already sorted
+            if(nums[low] <= nums[high]) {
+                ans = Math.min(ans, nums[low]);
+                break;
+            }
+
+            int mid = low + (high - low) / 2;
+
+            // Left half is sorted
+            if(nums[low] <= nums[mid]) {
+                ans = Math.min(ans, nums[low]);
+                low = mid + 1;
+            }
+
+            // Right half is sorted, rotation point in left half
+            else {
+                ans = Math.min(ans, nums[mid]);
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+}
