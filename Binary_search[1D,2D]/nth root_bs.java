e5582b9cb906b92dd0721265ecb53750f3ce4445
greeshmaa965-cell <greeshmaa965@gmail.com>
2026-06-04 09:55:56 +0530

    Implemented nth root binary search algorithm

---
+++ Binary_search[1D,2D]/nth root_bs.java
@@ -0,0 +1,49 @@
+class Solution {
+
+    private int power(int mid, int n, int m) {
+
+        long ans = 1;
+
+        for(int i = 1; i <= n; i++) {
+
+            ans *= mid;
+
+            if(ans > m) {
+                return 2;
+            }
+        }
+
+        if(ans == m) {
+            return 1;
+        }
+
+        return 0;
+    }
+
+    public int NthRoot(int n, int m) {
+
+        int low = 1;
+        int high = m;
+
+        while(low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            int val = power(mid, n, m);
+
+            if(val == 1) {
+                return mid;
+            }
+
+            else if(val == 0) {
+                low = mid + 1;
+            }
+
+            else {
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
