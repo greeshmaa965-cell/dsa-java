710a44714b1741f154cd642f534c27c13851daa2
greeshmaa965-cell <greeshmaa965@gmail.com>
2026-06-28 16:31:45 +0530

    Implemented anagram check with brute and optimal solutions
    Added two implementations of the anagram check: a brute force solution using sorting and an optimal solution using frequency counting.

---
+++ Strings/Anagram_or_not.java
@@ -0,0 +1,40 @@
+////////////////////brute solution////////////
+import java.util.Arrays;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length())
+            return false;
+
+        char[] a = s.toCharArray();
+        char[] b = t.toCharArray();
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        return Arrays.equals(a, b);
+    }
+}
+//////////////////optimal solution/////////)////
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length())
+            return false;
+
+        int[] freq = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+            freq[t.charAt(i) - 'a']--;
+        }
+
+        for (int count : freq) {
+            if (count != 0)
+                return false;
+        }
+
+        return true;
+    }
+}
