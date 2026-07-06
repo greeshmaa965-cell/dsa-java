1528b2a1a4bd8ca964657c8f438a169c3709f7e9
greeshmaa965-cell <greeshmaa965@gmail.com>
2026-07-06 20:28:05 +0530

    Implemented insertAtEnd method for linked list

---
+++ Insertion_at_headll.java
@@ -0,0 +1,35 @@
+//////////////////////////////
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class Solution {
+
+    public static Node insertAtEnd(Node head, int val) {
+
+        Node newNode = new Node(val);
+
+        // If list is empty
+        if (head == null) {
+            return newNode;
+        }
+
+        Node temp = head;
+
+        // Reach the last node
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+
+        // Link last node to new node
+        temp.next = newNode;
+
+        return head;
+    }
+}
