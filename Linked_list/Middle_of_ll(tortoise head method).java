5971003bcd46816312b0f8c27c3abcd9dd36818c
greeshmaa965-cell <greeshmaa965@gmail.com>
2026-07-13 21:21:15 +0530

    Implemented tortoise and hare method to find middle node

---
+++ Linked_list/Middle_of_ll(tortoise head method).java
@@ -0,0 +1,25 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class Solution {
+
+    public static Node middleNode(Node head) {
+
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+}
