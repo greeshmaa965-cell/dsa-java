47ead03cedf02c9a28dc08ad03935706706da00c
greeshmaa965-cell <greeshmaa965@gmail.com>
2026-07-17 19:53:52 +0530

    Implemented cycle detection in linked list

---
+++ Linked_list/Find_the_starting_point_ll.java
@@ -0,0 +1,30 @@
+class Solution {
+    public ListNode detectCycle(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        // Phase 1: Detect if a cycle exists
+        while (fast != null && fast.next != null) {
+
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+
+                // Phase 2: Find the starting node of the cycle
+                slow = head;
+
+                while (slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+
+                return slow;
+            }
+        }
+
+        // No cycle
+        return null;
+    }
+}
