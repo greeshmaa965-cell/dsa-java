83e8a0e44161ba6d711c74f451307be09c3f99ed
greeshmaa965-cell <greeshmaa965@gmail.com>
2026-07-27 22:02:15 +0530

    Implemented addOne method for linked list representation

---
+++ Linked_list/Add_one_to_number_represented_ll.java
@@ -0,0 +1,29 @@
+class Solution {
+
+    public Node addOne(Node head) {
+
+        int carry = helper(head);
+
+        if (carry == 1) {
+            Node newHead = new Node(1);
+            newHead.next = head;
+            return newHead;
+        }
+
+        return head;
+    }
+
+    private int helper(Node node) {
+
+        if (node == null)
+            return 1;
+
+        int carry = helper(node.next);
+
+        int sum = node.data + carry;
+
+        node.data = sum % 10;
+
+        return sum / 10;
+    }
+}
