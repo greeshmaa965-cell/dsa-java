73adc21890b5979fb744fe23cc88d767e89216d3
greeshmaa965-cell <greeshmaa965@gmail.com>
2026-08-20 21:19:10 +0530

    Added method to count subarrays with average above threshold

---
+++ Sliding_window/no_of_subarray_sizek_avg>threshold.java
@@ -0,0 +1,25 @@
+class Solution
+ {
+    public int numOfSubarrays(int[] arr, int k, int threshold)
+    {
+        int left=0;
+        int sum=0;
+       
+        int count=0;
+        for(int right=0;right<arr.length;right++)
+        {
+            sum+=arr[right];
+            if(right-left+1==k)
+            {
+                
+                if(sum>=threshold*k)
+                {
+                    count++;
+                }
+                sum-=arr[left];
+                left++;
+            }
+        }
+        return count;
+    }
+}
