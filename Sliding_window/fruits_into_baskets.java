ebef9216553a9d735dd683c92b818d3338267121
greeshmaa965-cell <greeshmaa965@gmail.com>
2026-08-23 16:02:47 +0530

    Implemented totalFruit method for max subarray

---
+++ Sliding_window/fruits_into_baskets.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int totalFruit(int[] fruits) 
+    {
+        //max subarray with exactly 2 distinct elements
+        int left=0;int maxlen=0;
+        HashMap<Integer,Integer>map=new HashMap<>();
+        for(int right=0;right<fruits.length;right++)
+        {
+            map.put(fruits[right],map.getOrDefault(fruits[right],0)+1);
+            while(map.size()>2)
+            {
+                map.put(fruits[left],map.get(fruits[left])-1);
+                if(map.get(fruits[left])==0)
+                {
+                    map.remove(fruits[left]);
+                }
+                left++;
+            }
+            maxlen=Math.max(maxlen,right-left+1);
+
+        }
+        return maxlen;
+    }
+}
