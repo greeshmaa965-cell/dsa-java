080e7f30aa0e6f969f3da9bb42a142731a1e201c
greeshmaa965-cell <greeshmaa965@gmail.com>
2026-08-23 16:23:55 +0530

    Added sliding window solution for max consecutive ones
    Implemented sliding window approach to find maximum consecutive ones in an array.

--- Arrays/max consecutive 1's .java
+++ Arrays/max consecutive 1's .java
@@ -24,6 +24,32 @@
 //       return max;
 //     }
 // }
+
+
+
+
+////////////////////////////////////////////sliding window//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+     class Solution 
+{
+    public int findMaxConsecutiveOnes(int[] nums)
+    {
+        int left=0;int maxlen=0;
+      
+        for(int right=0;right<nums.length;right++)
+        {
+     
+            if(nums[right]==0)
+            {
+        
+               left=right+1;
+            }
+            maxlen=Math.max(maxlen,right-left+1);
+        }
+        return maxlen;
+    }
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
                            /*OPTIMAL SOLUTION*/
                            class Solution {
     public int findMaxConsecutiveOnes(int[] nums)
