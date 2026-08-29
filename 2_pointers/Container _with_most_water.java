4c30243459072195ac84ce72c1c9488c208e14a7
greeshmaa965-cell <greeshmaa965@gmail.com>
2026-08-29 21:03:15 +0530

    Implemented maxArea method for Container with Most Water

---
+++ 2_pointers/Container _with_most_water.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxArea(int[] height) {
+
+        int left = 0;
+        int right = height.length - 1;
+        int max = 0;
+
+        while (left < right) {
+
+            int water = Math.min(height[left], height[right])
+                        * (right - left);
+
+            max = Math.max(max, water);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return max;
+    }
+}
