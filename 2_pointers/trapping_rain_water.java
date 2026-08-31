98d078ee5da7b0c9e0141a8d7a21a5cb76438579
GREESHMA ASWARTHAGARI <greeshmaa965@gmail.com>
2026-08-31 20:27:18 +0530

    Refactored trapping rainwater algorithm
    Refactor rainwater trapping logic to use count instead of water variable.

--- 2_pointers/trapping_rain_water.java
+++ 2_pointers/trapping_rain_water.java
@@ -1,38 +1,31 @@
 class Solution {
+
     public int trap(int[] height) {
 
         int left = 0;
         int right = height.length - 1;
 
-        int leftMax = 0;
-        int rightMax = 0;
-
-        int water = 0;
+        int rightmax = 0;
+        int leftmax = 0;
+        int count = 0;
 
         while (left < right) {
 
-            if (height[left] <= height[right]) {
+            leftmax = Math.max(leftmax, height[left]);
+            rightmax = Math.max(rightmax, height[right]);
 
-                if (height[left] >= leftMax) {
-                    leftMax = height[left];
-                } else {
-                    water += leftMax - height[left];
-                }
+            if (leftmax < rightmax) {
 
+                count += leftmax - height[left];
                 left++;
 
             } else {
 
-                if (height[right] >= rightMax) {
-                    rightMax = height[right];
-                } else {
-                    water += rightMax - height[right];
-                }
-
+                count += rightmax - height[right];
                 right--;
             }
         }
 
-        return water;
+        return count;
     }
 }
