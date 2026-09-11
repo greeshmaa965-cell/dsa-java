e3620f41df6c78b8d58224b66ff3ef74abb540a2
GREESHMA ASWARTHAGARI <greeshmaa965@gmail.com>
2026-09-11 22:59:22 +0530

    Implemented palindrome linked list check

---
+++ slow_fast_pointers/palindrome_ll.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        // Find middle
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Reverse from slow
+        ListNode prev = null;
+        ListNode curr = slow;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        // Compare
+        ListNode left = head;
+        ListNode right = prev;
+
+        while (right != null) {
+            if (left.val != right.val) {
+                return false;
+            }
+
+            left = left.next;
+            right = right.next;
+        }
+
+        return true;
+    }
+}
